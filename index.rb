6b7c752b9707967b761a0170334a2391f1c94a5c
Carter Rabasa <carter.rabasa@gmail.com>
2014-09-02 17:11:41 -0700

    some small tweaks

--- index.rb
+++ index.rb
@@ -1,21 +1,9 @@
 require 'sinatra'
 require 'twilio-ruby'
 
-get '/sms' do
+get '/sms' do 
   response = Twilio::TwiML::Response.new do |r|
-  	r.Message 'Hey, thanks for helping me out with my demo. Ping me anytime @CarterRabasa if you have any questions.'
-  end
-  content_type 'text/xml'
-  response.text
-end
-
-post '/random' do
-  client = Twilio::REST::Client.new ENV['TWILIO_ACCOUNT_SID'], ENV['TWILIO_AUTH_TOKEN']
-  msg = client.account.messages.list(to: '+15039266296').sample
-  response = Twilio::TwiML::Response.new do |r|
-  	r.Dial callerId: '+15039266296' do |d|
-  		d.Number msg.from
-  	end
+  	r.Message 'Thanks for helping me out with my presentation, PDX people are awesome :)'
   end
   content_type 'text/xml'
   response.text
@@ -25,4 +13,16 @@ get '/token' do
   capability = Twilio::Util::Capability.new ENV['TWILIO_ACCOUNT_SID'], ENV['TWILIO_AUTH_TOKEN']
   capability.allow_client_outgoing 'APaa39bb62e404d184ca4d794735b30dd9'
   capability.generate
+end
+
+get '/random' do
+  client = Twilio::REST::Client.new ENV['TWILIO_ACCOUNT_SID'], ENV['TWILIO_AUTH_TOKEN']
+  message = client.account.messages.list(to: '+15033768572').sample
+  response = Twilio::TwiML::Response.new do |r|
+  	r.Dial callerId: '+15033768572' do |d|
+  		d.Number '202-285-6865' #message.from
+  	end
+  end
+  content_type 'text/xml'
+  response.text 
 end
--- public/js/visualizemediastream.js
+++ public/js/visualizemediastream.js
@@ -1,8 +1,15 @@
+/**
+ * Pass in a MediaStream and its volume will be visualized
+ */
 function visualizeMediaStream(stream) {
 
+	// the context in which all the audio processing will take place
 	audioContext = new webkitAudioContext();
+	// the analyser can expose time and frequency data
 	analyser = audioContext.createAnalyser();
+	// connect our WebRTC media stream to the audio context
 	microphone = audioContext.createMediaStreamSource(stream);
+	// the script processor will let us sample the audio 
 	scriptProcessor = audioContext.createScriptProcessor(2048, 1, 1);
 
 	analyser.smoothingTimeConstant = 0.3;
